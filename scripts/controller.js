73fecb554390e39900a42af07ca5e8fdb66577da
adesanto <aarondesanto@gmail.com>
2015-11-23 13:16:07 -0500

    editing angular styles trying to prep for conversion to mobile

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,7 @@
 var app = angular.module('homeModule');
 
-app.controller('mainControl', function($scope, mainService) {
+app.controller('mainControl', function($scope, portfolioSrv) {
 
-  $scope.projects = [
-    {
-      name: "Project 1",
-      content: "Some Content"
-    },
-    {
-      name: "Project 1",
-      content: "Some Content"
-    }
-  ];
-  $scope.languages = mainService.languages;
+  $scope.projects = portfolioSrv.projects;
 
 });
--- scripts/service.js
+++ scripts/service.js
@@ -1,5 +1,47 @@
 var app = angular.module('homeModule');
 
-app.factory('mainService', function() {
-  return { languages: ["HTML", "CSS", "JS", "Angular", "jQuery"] };
+app.factory('portfolioSrv', function() {
+  return { projects: [
+      {
+        name: "Contentment",
+        content: "Some Content",
+        langs: ["JavaScript", "jQuery"],
+        link: 'https://github.com/aarondesanto/Contentment',
+        thumb: 'images/thumb1.jpg',
+        alt: 'TEXT'
+      },
+      {
+        name: "Seats",
+        content: "Some Content",
+        langs: ["HTML", "CSS", "JavaScript", "jQuery"],
+        link: 'https://github.com/aarondesanto/seats',
+        thumb: 'images/thumb1.jpg',
+        alt: 'TEXT'
+      },
+      {
+        name: "ShoppStar",
+        content: "Some More Content",
+        langs: ["HTML", "CSS", "JavaScript", "Angular", "jQuery"],
+        link: 'https://github.com/aarondesanto/ShopWithAngular',
+        thumb: 'images/thumb1.jpg',
+        alt: 'TEXT'
+      },
+      {
+        name: "SHAPES THING",
+        content: "Some Content",
+        langs: ["HTML", "CSS"],
+        link: 'https://github.com/aarondesanto/',
+        thumb: 'images/thumb1.jpg',
+        alt: 'TEXT'
+      },
+      {
+        name: "ANIMATION",
+        content: "Some Content",
+        langs:  ["HTML", "CSS", "JavaScript", "jQuery"],
+        link: 'https://github.com/aarondesanto/',
+        thumb: 'images/thumb1.jpg',
+        alt: 'TEXT'
+      }
+    ]
+  };
 });
